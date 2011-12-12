cc15ee8f31da6a7574b94b385b0b8d13c114dfc7
hide2 <hideto.bj@gmail.com>
2011-12-13 03:02:06 +0800

    8

--- h5bp.rb
+++ h5bp.rb
@@ -7,7 +7,9 @@ inside('app/assets/stylesheets/') do
   FileUtils.touch 'application.css'
 end
 prepend_to_file 'app/assets/stylesheets/application.css' do
-  " /*
+  "
+
+/*
  * This is a manifest file that'll automatically include all the stylesheets available in this directory
  * and any sub-directories. You're free to add application-wide styles to this file and they'll appear at
  * the top of the compiled file, but it's generally better to create a new file per style scope.
@@ -16,11 +18,12 @@ prepend_to_file 'app/assets/stylesheets/application.css' do
  *= require application-post
  *= require_tree .
 */
+
 "
 end
 get "https://github.com/hide2/rails-h5bp/raw/master/assets/stylesheets/style.css", "app/assets/stylesheets/application-pre.css"
 get "https://github.com/hide2/rails-h5bp/raw/master/assets/stylesheets/style.css", "app/assets/stylesheets/application-post.css"
-gsub_file 'app/assets/stylesheets/application-pre.css', /\n*\/\* ==\|== media queries.* /m, ''
+gsub_file 'app/assets/stylesheets/application-pre.css', /\n+\/\* ==\|== media queries.* /m, ''
 gsub_file 'app/assets/stylesheets/application-post.css', /\A.*?(==\|== primary styles).*?(\*\/){1}\n*/m, ''
 
 # Update application.html.erb with HTML5 Boilerplate index.html content
@@ -41,8 +44,10 @@ gsub_file 'app/views/layouts/application.html.erb', /<meta charset="utf-8">/ do
 end
 gsub_file 'app/views/layouts/application.html.erb', /<div role="main">[\s\S]*<\/div>/ do
   "<div role=\"main\">
-    <%= yield %>
+
+<%= yield %>
+
   </div>
   "
 end
-gsub_file 'app/views/layouts/application.html.erb', /<!-- scripts[\s\S]*end scripts -->/, '<%= javascript_include_tag "application" %>'
+gsub_file 'app/views/layouts/application.html.erb', /<!-- Grab Google CDN[\s\S]*end scripts -->/, '<%= javascript_include_tag "application" %>'
